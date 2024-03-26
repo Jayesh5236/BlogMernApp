208b6e66b783062c0d35ba62cac2580af3bc7b98
Jayesh5236 <jayesh5236@gmail.com>
2024-03-26 17:19:03 +0530

    Created Test Api Route

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import chalk from "chalk";
 import dbConnect from "./Database/dbConnect.js";
 import morgan from "morgan";
+import userController from "./Controllers/Users/index.js"
 
 // Configure Dotenv
 dotenv.config();
@@ -17,6 +18,12 @@ app.use(express.json()); //Bodyparser
 app.use(morgan("dev"));
 
 const port = process.env.PORT || 5001;
+
+
+app.use("/user",userController)
+app.get("/test", (req, res) => {
+  res.status(200).json({ message: "Backend Is Working" });
+});
 
 app.listen(port, () => {
   console.log(chalk.bgMagenta.bold.white(`Server Running On ${port} `));

8cb9ed775a7b5a08ba6557069ee39241f83beadb
Jayesh5236 <jayesh5236@gmail.com>
2024-04-07 17:06:06 +0530

    added comments

--- backend/index.js
+++ backend/index.js
@@ -3,25 +3,23 @@ import dotenv from "dotenv";
 import chalk from "chalk";
 import dbConnect from "./Database/dbConnect.js";
 import morgan from "morgan";
-import userController from "./Controllers/Users/index.js"
+import userController from "./Controllers/Users/index.js";
 
 // Configure Dotenv
-dotenv.config();
+dotenv.config(); //loads Environment variables from the .env file
 
 // Database Config
-dbConnect();
+dbConnect(); //MongoDb Database Connection
 
-const app = express();
+const app = express(); //Intialize Express Web Framework
 
 // Middlewares
-app.use(express.json()); //Bodyparser
-app.use(morgan("dev"));
+app.use(express.json()); //Bodyparser In JSON Response
+app.use(morgan("dev")); // Logs incoming requests to the console (useful for development.
 
 const port = process.env.PORT || 5001;
 
-
-app.use("/api/user",userController)
-
+app.use("/api/user", userController);
 
 app.get("/test", (req, res) => {
   res.status(200).json({ message: "Backend Is Working" });
